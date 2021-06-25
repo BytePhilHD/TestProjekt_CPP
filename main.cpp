cfe13ec8523609320b12c0c269ed01df73b34780
BytePhil <phitho2018@gmail.com>
2021-06-25 23:46:02 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+std::string test() {
+    std::string string = "haa";
+    return string;
+}
+
+int main2() {
+    std::cout << "Hello, World! " << test() << std::endl;
+    std::cout << "He" << std::endl;
+    std::string input;
+    std::cin >> input;
+    return 0;
+}
---
+++ main2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by phith on 25.06.2021.
+//
+#include "iostream"
+
+int main() {
+    std::cout << "He Chris du Keck. Tippen sie was ein alla" << std::endl;
+    std::string input;
+    std::cin >> input;
+    std::cout << "Hallo, " << input;
+
+    return 0;
+}
